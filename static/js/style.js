02c3a60e2b107922f253e0abeed415cb2314ddd9
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-04-27 15:53:54 +0700

    add scrollspy

--- static/js/style.js
+++ static/js/style.js
@@ -15,6 +15,28 @@ window.addEventListener("scroll", function () {
 
 (function () {
     'use strict'
+    var section = document.querySelectorAll(".section");
+    var sections = {};
+    var i = 0;
+
+    Array.prototype.forEach.call(section, function (e) {
+        sections[e.id] = e.offsetTop;
+    });
+
+    window.onscroll = function () {
+        var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        for (i in sections) {
+            if (sections[i] <= scrollPosition) {
+                document.querySelector('.active').classList.remove('active');
+                document.querySelector('a[href*=' + i + ']').classList.add('active');
+            }
+        }
+    };
+})();
+
+(function () {
+    'use strict'
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
